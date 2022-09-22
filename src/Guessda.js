bf9be2b91aa5d82e0ad76255a5c11ff2d431841d
Arad Eytan <dara9991@gmail.com>
2022-09-22 20:15:54 +0300

    first commit

---
+++ src/Guessda.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Guessda = () => {
+    return (
+        <>
+            <div className='heading'>
+                <p>GUESSDA.COM</p>
+            </div>
+            <div className='container'>
+                <img src='https://picsum.photos/400/225' alt='...'/>
+                <br/>
+                <input />
+            </div>
+        </>
+
+    )
+}
+
+export default Guessda
