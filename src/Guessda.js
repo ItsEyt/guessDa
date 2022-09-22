ac447f256f307c2dae394b831f854d67d9ec69df
Arad Eytan <dara9991@gmail.com>
2022-09-22 22:52:55 +0300

    css updates

--- src/Guessda.js
+++ src/Guessda.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Guessda = () => {
+
+    const [guess, setGuess] = useState('')
+
+    const handleKeyDown = event => {
+        console.log('User pressed: ', event.key);
+        if (event.key === 'Enter') {
+            console.log('Enter key pressed ✅');
+        }
+    };
+
     return (
         <>
             <div className='heading'>
                 <p>GUESSDA.COM</p>
             </div>
             <div className='container'>
-                <img src='https://picsum.photos/400/225' alt='...'/>
-                <br/>
-                <input />
+                <img src='https://picsum.photos/400/225' alt='...' />
+                <br />
+                <input value={guess}
+                    onChange={(evt) => setGuess(evt.target.value)}
+                    onKeyDown={handleKeyDown} />
             </div>
         </>
 
